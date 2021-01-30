5060da18910539ffde91b48fdd4d127f4036552f
Sofiya <katutuzun@gmail.com>
2021-01-30 23:19:18 +0200

    second project

--- JS/Практичне завдання 2/js/script.js
+++ JS/Практичне завдання 2/js/script.js
@@ -5,6 +5,58 @@
 
 // 2
 
-let myAge = 11;
-let myFriendsAge = 15;
-alert('Ours age: ', );
+// let myAge = 11;
+// let myFriendsAge = 15;
+// let ourAge = myFriendsAge + myAge;
+// alert('Our age: ' + ourAge);
+
+// 3
+
+// let a;
+// let b;
+// console.log(a*b);
+
+// 4 
+
+// let a1 = 5 + 3,
+//     a2 = 5 - 3,
+//     a3 = 5 * 3,
+//     a4 = 5 / 3,
+//     a5 = 3 - 5;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+// 5
+
+// let a6 = 5 % 3,
+//     a7 = 3 % 5,
+//     a8 = 5 + '3',
+//     a9 = '5' - 3,
+//     a10 = 75 + 'кг';
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+
+// 6
+
+// let height = 23; // см
+// let width = 10; // см
+// const S = height * width;
+
+// 7
+
+// let heightC = 10; // м
+// let dC = 4; // м
+// const V = heightC * dC;
+
+// 8
+
+// let n = 3;
+// let m = 4;
+// let k = Math.sqrt((n ** 2) + (m ** 2));
+// console.log(k);
---
+++ JS/Практичне завдання 4/js/script.js
@@ -0,0 +1,20 @@
+// 1
+
+// let today = 24;
+// if (today === 1) {
+//     console.log("сьогодні перше число");
+// } else {
+//     console.log(today);
+// }
+
+// 2
+
+// let today = 24;
+// if (today % 2 === 0) {
+//     console.log("сьогодні парне число");
+// } else {
+//     console.log("upppss");
+// }
+
+// 3
+
